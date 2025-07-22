1b632972742e186c3301b565c55c770383dada32
William Tobias Keating <williamtobiaskeating@Williams-MacBook-Pro-2.local>
2025-07-22 12:42:56 +0200

    fix: move 'use client' to top of login page for Next.js compliance

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-dynamic";
+
+'use client';
+import React from 'react';
+import LoginPageInner from './LoginPageInner';
+
+export default function LoginPage() {
+  return <LoginPageInner />;
+} 
